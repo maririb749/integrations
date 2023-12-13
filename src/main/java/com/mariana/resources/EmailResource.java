5c9201fdc9af15a501befa8bab11b36a3332f63f
Mariana Santos <maririb51@gmail.com>
2023-12-13 14:46:06 +0000

    Enviado primeiro email com SendGrid

--- src/main/java/com/mariana/resources/EmailResource.java
+++ src/main/java/com/mariana/resources/EmailResource.java
@@ -1,5 +1,6 @@
 package com.mariana.resources;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -7,13 +8,18 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.mariana.dto.EmailDTO;
+import com.mariana.service.EmailService;
 
 @RestController
-@RequestMapping(value ="/emails")
+@RequestMapping(value = "/emails")
 public class EmailResource {
-	
+
+	@Autowired
+	private EmailService service;
+
 	@PostMapping
-	public ResponseEntity<Void> send (@RequestBody EmailDTO dto){
+	public ResponseEntity<Void> send(@RequestBody EmailDTO dto) {
+		service.sendEmail(dto);
 		return ResponseEntity.noContent().build();
 	}
 
